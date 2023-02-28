44fa1b90fbd44515756352ce7460acdfb372f805
Idan Maman <90776557+idanmaman2@users.noreply.github.com>
2023-02-28 15:11:54 +0200

    EX0

--- main.rkt
+++ main.rkt
@@ -1,3 +1,69 @@
-#lang racket/base
-(print "cool")
-(define (r ls ) ( for ( [i ls ] ) (display (* i i ) ) ))
+#lang racket/base 
+(require racket/string)
+(require racket/list)
+(define totalbuy 0 )
+(define totalcell 0)
+
+
+
+(define argFolder 
+        (vector*-ref (current-command-line-arguments) 
+                    0))
+
+
+(define fileName (string-append 
+                    (last 
+                        (string-split argFolder "/") ) 
+                    ".asm"))
+
+
+(define (fileAnalyze path  name )
+(define file (open-input-file (string-append path "/" name )))
+(define (analyze fileLine)
+    (define (HandleBuy productName amount price) 
+     (define mul  (* (string->number amount) (string->number price)) )
+     (set! totalbuy (+ totalbuy mul))
+     (string-append "### BUY " productName " ###\n" (number->string mul) "\n"))
+     
+    (define (HandleCell productName amount price)
+        (define mul (* (string->number amount) (string->number price)) ) 
+        (set! totalcell (+ totalcell mul))
+        (string-append "$$$ CELL " productName " $$$\n" (number->string mul ) "\n"))
+    
+    (let* (
+        [splitted (string-split fileLine " ")]
+        [command (car splitted)]
+        )
+    (cond 
+        [(string=? "buy" command) (HandleBuy (second splitted) (third splitted) (fourth splitted))]
+        [(string=? "cell" command) (HandleCell (second splitted) (third splitted) (fourth splitted))]
+        [else ""]
+        )
+    ) 
+
+
+ ) 
+ (define ( looped fileo )
+            (let ([line (read-line fileo) ])
+            (cond [(eof-object? line) "" ]
+                   [else (string-append (analyze line ) (looped fileo) )]
+            )
+        
+         ) )
+
+ (string-append  name "\n" (looped file))
+)
+
+
+(define outputfile (open-output-file (string-append argFolder "/" fileName) 
+        #:exists 'replace
+         #:replace-permissions? #t
+        ))
+(for  ([ i (map path->string (directory-list argFolder))]     
+    #:when (string-suffix?  i ".vm") )
+    (display (fileAnalyze argFolder i) outputfile)
+)
+(display "TOTAL BUY:" outputfile)
+(display totalbuy outputfile)
+(display "\nTOTAL CELL:" outputfile)
+(display totalcell outputfile)
---
+++ test-input/test-input.asm
@@ -0,0 +1,30 @@
+InputA.vm
+### BUY bread ###
+13.5
+### BUY cheese ###
+10.6
+$$$ CELL cheese $$$
+85.5
+### BUY milk ###
+24.8
+$$$ CELL chocolate $$$
+71.39999999999999
+### BUY milk ###
+22.400000000000002
+$$$ CELL chocolate $$$
+11.0
+InputB.vm
+### BUY chocolate ###
+12.5
+$$$ CELL eggs $$$
+621.0
+### BUY cheese ###
+10.6
+$$$ CELL sugar $$$
+82.5
+$$$ CELL flour $$$
+25.2
+### BUY milk ###
+11.100000000000001
+TOTAL BUY:105.5
+TOTAL CELL:896.6
